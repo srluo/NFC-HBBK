4c179e3918867fdbd9d6d5a9ab2166ac25b7d648
srluo <sr.luo2@gmail.com>
2025-10-26 12:32:14 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+// next.config.js (ESM syntax)
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // 這一行確保使用 Node runtime
+  experimental: {
+    runtime: "nodejs",
+  },
 };
+
+export default nextConfig;
