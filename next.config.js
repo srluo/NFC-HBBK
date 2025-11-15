9121891d6046d5c25b0b940ed4c5985dcd7486aa
srluo <sr.luo2@gmail.com>
2025-11-15 17:49:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   turbopack: {
-    root: __dirname,
+    root: "./",
   },
-  serverExternalPackages: ["pdfkit", "fontkit"],
 };
-
-export default nextConfig;
